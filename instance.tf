b9cc1d5711c8613e28090ed7dfa093ebf375b06f
esiddiqi <siddiquiemaad@gmail.com>
2022-04-17 16:50:36 +0530

    Created Output data

--- instance.tf
+++ instance.tf
@@ -1,42 +1,42 @@
-resource "aws_key_pair" "ec2-terrademo-key" {
+#resource "aws_key_pair" "ec2-terrademo-key" {
 
- key_name   = "terra-ec2-key"
- public_key = file("ec2-terra-ssh-key-demo.pub")
+# key_name   = "terra-ec2-key"
+# public_key = file("ec2-terra-ssh-key-demo.pub")
 
-}
+#}
 
 resource "aws_instance" "terra-instance" {
 
  ami = var.AMIS[var.REGION]
  instance_type = "t2.micro"
  availability_zone = var.ZONE1
- key_name = aws_key_pair.ec2-terrademo-key.key_name
+# key_name = aws_key_pair.ec2-terrademo-key.key_name
  vpc_security_group_ids = ["sg-05ed6f021b22c2359"]
  tags = {
    Name = "terra-instance"
  }
 
- provisioner "file" {
-   source = "web.sh"
-   destination = "/tmp/web.sh"
+ #provisioner "file" {
+ #  source = "web.sh"
+ #  destination = "/tmp/web.sh"
 
- }
+# }
 
- provisioner remote-exec{
-   inline = [
-       "chmod u+x /tmp/web.sh",
-       "sudo /tmp/web.sh"
+ #provisioner remote-exec{
+#   inline = [
+ #      "chmod u+x /tmp/web.sh",
+#       "sudo /tmp/web.sh"
 
-   ]
- }
- connection {
-   user = "ec2-user"
-   private_key = file("ec2-terra-ssh-key-demo")
-   host = self.public_ip
+#   ]
+# }
+# connection {
+#   user = "ec2-user"
+#   private_key = file("ec2-terra-ssh-key-demo")
+#   host = self.public_ip
 
- }
+# }
 
-}
+#}
 
 
 
